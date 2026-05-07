b7a66e7c2d67d134194c6cf9afe872e50118c3a8
GAELSILLOOOO <a23310295@ceti.mx>
2026-05-06 19:36:06 -0600

    metodologias_agiles
    metodologias_agiles

---
+++ metodologias_agile.py
@@ -0,0 +1,8 @@
+#2. Metodologías Ágiles (Iterativas)
+#Surgieron como respuesta a la rigidez de la cascada. Se centran en la entrega rápida de valor, la flexibilidad ante los cambios y la colaboración constante.
+
+#Scrum: El más popular. Se trabaja en ciclos cortos llamados Sprints (de 1 a 4 semanas). Al final de cada sprint, se entrega una parte funcional del software. Tiene roles definidos como el Scrum Master y el Product Owner.
+
+#Kanban: Se enfoca en la visualización del flujo de trabajo. Utiliza tableros con columnas (Por hacer, En progreso, Hecho) para evitar cuellos de botella y gestionar la capacidad del equipo en tiempo real.
+
+#XP (Extreme Programming): Enfocada puramente en la calidad del código. Promueve prácticas como la programación en parejas (pair programming) y las pruebas constantes.

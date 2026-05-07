eb73784731838b32048566ee693fe56b873cf5ce
GAELSILLOOOO <a23310295@ceti.mx>
2026-05-06 19:58:20 -0600

    ejemplo_dev_ops
    ejemplo_dev_ops

---
+++ Ejemplos/devops.py
@@ -0,0 +1,7 @@
+#DevOps
+
+#Proyecto: Una plataforma de streaming como Netflix.
+
+#El Enfoque: Automatización total.
+
+#En Python: Escribes scripts que prueban automáticamente tu código (PyTest). Cada vez que haces un cambio, un sistema automático detecta el error, lo corrige o lo sube a la nube sin que un humano toque un botón.

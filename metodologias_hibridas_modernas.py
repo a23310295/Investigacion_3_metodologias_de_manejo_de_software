952da177214ebabfed9c7b776476b33f7fdb07f6
GAELSILLOOOO <a23310295@ceti.mx>
2026-05-06 19:37:44 -0600

    metodologias_hibridas_modernas
    metodologias_hibridas_modernas

---
+++ metodologias_hibridas_modernas.py
@@ -0,0 +1,8 @@
+#Metodologías Híbridas y Modernas
+#Combinan lo mejor de varios mundos o se enfocan en la integración técnica.
+
+#Lean Software Development: Inspirada en la manufactura de Toyota. Su objetivo principal es eliminar el "desperdicio" (todo lo que no aporta valor al cliente) y optimizar el flujo.
+
+#Spiral (Espiral): Combina elementos de diseño y prototipado por etapas. Es ideal para proyectos de alto riesgo, ya que incluye un análisis de riesgos detallado en cada ciclo.
+
+#DevOps: Más que una metodología de gestión, es una cultura que une el Desarrollo (Dev) y las Operaciones (Ops). Busca la automatización y la entrega continua mediante herramientas de CI/CD.
